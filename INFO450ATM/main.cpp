bd4c9ce2f5cbd99aa7acbd83a1323633287acbe3
Abbie Thorne <thorneal@vcu.edu>
2019-09-19 23:05:45 -0400

    Initial Commit

---
+++ INFO450ATM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  INFO450ATM
+//
+//  Created by Abbie Thorne on 9/19/19.
+//  Copyright © 2019 Abbie Thorne. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
